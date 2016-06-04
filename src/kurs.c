64ea78da70a1528544e32c5ef959a622a2cdd082
Polina Arkashina <polly23101996@gmail.com>
2016-06-04 15:47:19 +0600

    Add kurs.c

--- src/kurs.c
+++ src/kurs.c
@@ -2,32 +2,53 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "total.h"
 
-double console(int ds, char *s)
-{	
+int console1 ()
+{
+	int ds,k=0,n=0,f=0, i=0, j=0;
+	char *c;
+	char s[10000];
+
+	printf("Введите текст:\n");
+	
+	do
+	{	
+		scanf("%c", &s[i]); 
+		i++;
+	}
+	while(s[i-1] != 0x9);
+	
+	s[i-1] = '\0';
+
+	ds = strlen(s);
+
 	char ms[ds*6][ds*6];
-	int ds_tmp = ds * 6;
-	char c[ds_tmp];
-	int n = 0, j = 0, k = 0, f = 0, i = 0;
-	while(n <= ds)
+
+	c = (char*) malloc(ds*sizeof(char));
+	while(n<=ds)
 	{
+
 		while(isalpha(s[n]))
 		{
-			ms[i][j] = s[n];
 
+			ms[i][j]=s[n];
 			j++; 
 			n++;
 		}
+
 		j = 0; 
 		i++; 
 		n++;
+
 	}
+
 	
 	do
 	{
 		f=0;
 		for(j=i-1;j>k;j--)
-		{
+		{ 
 			if (strcmp(ms[j],ms[j-1])<0)
 			{ 
 				strcpy(c,ms[j]);
@@ -38,14 +59,16 @@ double console(int ds, char *s)
 		}
 		k++;
 	}
-	while(f);
-	
-	printf("\nОтсортированные слова:\n");                  
+	while(f);                     
+	printf("Слова, отсортированные по алфавиту:\n");
 	for (j=0;j<i;j++)
 	{
-		if (ms[j][0])
-			printf(" %s\n", ms[j]);
-	}
 
-    return 3;
+		
+		if (ms[j][0])
+		printf(" %s\n", ms[j]);
+	}
+	free (c);
+
+    return EXIT_SUCCESS;
 }
